567990a68d59905d17cc36f38ae5c41b459c40be
jlarsen9 <44121263+jlarsen9@users.noreply.github.com>
2018-12-08 11:40:30 -0800

    Create getFirstCharacter.js

---
+++ getFirstCharacter.js
@@ -0,0 +1,11 @@
+//make a function that takes the string and returns the first character of the string
+
+function getFirstCharacter(sentence) {
+  var index = 0;
+  var firstCharacter = sentence.charAt(index);
+  sentence.charAt(index);
+  return firstCharacter;
+}
+
+var a = getFirstCharacter("hello");
+console.log(a);
